ea4012cc6519a575eafa51728b0c027908029c4f
서창열 <kongbaguni@gmail.com>
2021-10-06 15:26:51 +0900

    Initial Commit

---
+++ Shared/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Shared
+//
+//  Created by Changyeol Seo on 2021/10/06.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
